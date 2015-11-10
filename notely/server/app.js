ac2e7a012aefab0b69e760bea0d5ae729e46b6dc
jmscott <jmscottimd@gmail.com>
2015-11-10 10:41:35 -0500

    add simple express server sending hard-coded note data

---
+++ notely/server/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/notes', function(req, res){
+res.json([
+  {
+    title: 'hardcoded note',
+    body_html: 'this is the body of my hardcocded notes'
+  },
+  {
+    title: 'another note',
+    body_html: 'woo hoo noob'
+  }
+]);
+});
+
+app.listen(3000, function(){
+  console.log('Listening on http://localhost:3000');
+});
