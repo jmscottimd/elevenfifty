19606003d26c28de0ede616ac3af713ed6fa2e24
jmscott <jmscottimd@gmail.com>
2015-11-10 14:51:23 -0500

    retrieve notes from mongodb

--- notely/server/app.js
+++ notely/server/app.js
@@ -1,30 +1,32 @@
 var express = require('express');
 var app = express();
 
+// db connection
+var db = require('mongoose');
+db.connect('mongodb://jmscottmongo:mosdef@ds041623.mongolab.com:41623/notelydb');
+
+//define schema
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String,
+  updated_at: { type: Date, default: Date.now }
+})
+
+//create a model
+var Note = db.model('Note', NoteSchema);
+
+// allow CORS
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
+// list all notes
 app.get('/notes', function(req, res){
-res.json([
-  {
-    title: 'hardcoded note',
-    body_html: 'this is the body of my hardcocded notes'
-  },
-  {
-    title: 'another note',
-    body_html: 'woo hoo noob'
-  },
-  {
-    title: 'mooarr notes',
-    body_html: 'woo hoo!!'
-  },
-  {
-    title: 'reup my notes',
-    body_html: 'reup!!'
-  }
-]);
+// retrieve notes in collection from mongodb
+  Note.find().then(function(notes){
+    res.json(notes);
+  });
 });
 
 app.listen(3000, function(){
